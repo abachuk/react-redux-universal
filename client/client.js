44f2fe9043197776c415bf6bd2d8be54bc7af92a
Alex Bachuk <abachuk@gmail.com>
2016-09-03 10:42:16 -0400

    finishing postinstall, working

---
+++ client/client.js
@@ -0,0 +1,30 @@
+/**
+ * THIS IS THE ENTRY POINT FOR THE CLIENT, JUST LIKE server.js IS THE ENTRY POINT FOR THE SERVER.
+ */
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';;
+import {Provider} from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { ReduxAsyncConnect } from 'redux-async-connect';
+
+
+const component = (
+  <div>Hello World</div>
+);
+
+ReactDOM.render(
+  <Provider key="provider">
+    {component}
+  </Provider>,
+  dest
+);
+
+if (process.env.NODE_ENV !== 'production') {
+  window.React = React; // enable debugger
+
+  if (!dest || !dest.firstChild || !dest.firstChild.attributes || !dest.firstChild.attributes['data-react-checksum']) {
+    console.error('Server-side React render was discarded. Make sure that your initial render does not contain any client-side code.');
+  }
+}
